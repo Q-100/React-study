1bd147d0321eb616031db37c9ec8f49e64b1053c
Q-100 <opea5954@gmail.com>
2021-08-05 23:34:34 +0900

    Navbar add

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,7 @@ function Header() {
             <div className="button-nav">
                 <button >목록 추가하기</button>
                 <button>글쓰기</button>
+
             </div>
         </div>
     )
